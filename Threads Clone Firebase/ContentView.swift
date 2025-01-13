3f1182bb113258c70898b61e86f52c46dafd9672
Andrii Pashuta <andrii.pashuta@gmail.com>
2025-01-13 16:24:35 +0100

    Initial Commit

---
+++ Threads Clone Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Threads Clone Firebase
+//
+//  Created by Andrii Pashuta on 13/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Threads Clone Firebase/Threads_Clone_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_Clone_FirebaseApp.swift
+//  Threads Clone Firebase
+//
+//  Created by Andrii Pashuta on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Threads_Clone_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
